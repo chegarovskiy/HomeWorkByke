ff044108e79b5cf7d57dd8c63dc9978085dc7e4a
chegarovskiy <chegarovskiy.vlad@gmail.com>
2016-07-02 14:54:52 +0300

    Create project

---
+++ BykeFormsMyHomeTask/BykeFormsMyHomeTask/CoreForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace BykeFormsMyHomeTask
+{
+    partial class CoreForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsAdd = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsFile,
+            this.tsAbout,
+            this.tsExit});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(504, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tsFile
+            // 
+            this.tsFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsAdd,
+            this.tsSave});
+            this.tsFile.Name = "tsFile";
+            this.tsFile.Size = new System.Drawing.Size(37, 20);
+            this.tsFile.Text = "File";
+            // 
+            // tsAdd
+            // 
+            this.tsAdd.Name = "tsAdd";
+            this.tsAdd.Size = new System.Drawing.Size(98, 22);
+            this.tsAdd.Text = "Add";
+            // 
+            // tsSave
+            // 
+            this.tsSave.Name = "tsSave";
+            this.tsSave.Size = new System.Drawing.Size(98, 22);
+            this.tsSave.Text = "Save";
+            // 
+            // tsAbout
+            // 
+            this.tsAbout.Name = "tsAbout";
+            this.tsAbout.Size = new System.Drawing.Size(52, 20);
+            this.tsAbout.Text = "About";
+            // 
+            // tsExit
+            // 
+            this.tsExit.Name = "tsExit";
+            this.tsExit.Size = new System.Drawing.Size(37, 20);
+            this.tsExit.Text = "Exit";
+            this.tsExit.Click += new System.EventHandler(this.tsExit_Click);
+            // 
+            // CoreForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 348);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "CoreForm";
+            this.Text = "CoreForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CoreForm_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsFile;
+        private System.Windows.Forms.ToolStripMenuItem tsAdd;
+        private System.Windows.Forms.ToolStripMenuItem tsSave;
+        private System.Windows.Forms.ToolStripMenuItem tsAbout;
+        private System.Windows.Forms.ToolStripMenuItem tsExit;
+    }
+}
+
